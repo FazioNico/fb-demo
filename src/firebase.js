debcf85928aaa21f2f3a926ab6d13e99b7c387a8
FazioNico <contact@nicolasfazio.ch>
2026-09-15 12:38:54 +0200

    add firebase integration for user data retrieval

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./firebase.config";
+import { getDatabase } from "firebase/database";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
